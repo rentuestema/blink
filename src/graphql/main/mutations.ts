rentuestema/blink

commit fdb16ac0c80c8fe4b6f396d0af0ddf914f2c9129

chore: adding update username use case to apps (#886)
* chore: move setUsername to app

* fixing prettier

* chore: apply suggestions from code review

Co-authored-by: Arvin <17693119+vindard@users.noreply.github.com>

* chore: add missing import

Co-authored-by: Arvin <17693119+vindard@users.noreply.github.com>

src/graphql/main/schema.graphql
@@ -337,6 +337,9 @@ type Mutation {
   userRequestAuthCode(input: UserRequestAuthCodeInput!): SuccessPayload!
   userUpdateLanguage(input: UserUpdateLanguageInput!): UserUpdateLanguagePayload!
   userUpdateUsername(input: UserUpdateUsernameInput!): UserUpdateUsernamePayload!
+    @deprecated(
+      reason: "Username will be moved to @Handle in Accounts. Also SetUsername should be used instead of UpdateUpdate to reflect the idempotenty of Handles"
+    )
 }
 
 type MyUpdatesPayload {

src/graphql/root/mutation/user-update-username.ts
@@ -1,3 +1,5 @@
+import { Accounts } from "@app"
+import { mapError } from "@graphql/error-map"
 import { GT } from "@graphql/index"
 
 import UserUpdateUsernamePayload from "@graphql/types/payload/user-update-username"
@@ -15,17 +17,20 @@ const UserUpdateUsernameMutation = GT.Field({
   args: {
     input: { type: GT.NonNull(UserUpdateUsernameInput) },
   },
-  resolve: async (_, args, { wallet }) => {
+  deprecationReason:
+    "Username will be moved to @Handle in Accounts. Also SetUsername should be used instead of UpdateUsername to reflect the idempotency of Handles",
+  resolve: async (_, args, { domainAccount }: { domainAccount: Account }) => {
     const { username } = args.input
 
     if (username instanceof Error) {
       return { errors: [{ message: username.message }] }
     }
 
-    const result = await wallet.updateUsername({ username })
+    const result = await Accounts.setUsername({ username, id: domainAccount.id })
 
     if (result instanceof Error) {
-      return { errors: [{ message: result.message }] }
+      const appErr = mapError(result)
+      return { errors: [{ message: appErr.message || appErr.name }] }
     }
 
     return {

src/app/accounts/index.ts
@@ -10,6 +10,8 @@ export * from "./update-account-status"
 export * from "./update-business-map-info"
 export * from "./send-balance-to-accounts"
 export * from "./add-earn"
+export * from "./set-username"
+export * from "./username-available"
 
 const accounts = AccountsRepository()
 

src/app/accounts/set-username.ts
@@ -1,0 +1,28 @@
+import { usernameAvailable } from "@app/accounts"
+import { UsernameIsImmutableError, UsernameNotAvailableError } from "@domain/accounts"
+import { checkedToUsername } from "@domain/users"
+import { AccountsRepository } from "@services/mongoose"
+
+export const setUsername = async ({
+  id,
+  username,
+}: {
+  id: string
+  username: string
+}): Promise<Account | ApplicationError> => {
+  const checkedUsername = checkedToUsername(username)
+  if (checkedUsername instanceof Error) return checkedUsername
+
+  const accountsRepo = AccountsRepository()
+  const account = await accountsRepo.findById(id as AccountId)
+  if (account instanceof Error) return account
+
+  if (account.username !== undefined) return new UsernameIsImmutableError()
+
+  const isAvailable = await usernameAvailable(checkedUsername)
+  if (isAvailable instanceof Error) return isAvailable
+  if (!isAvailable) return new UsernameNotAvailableError()
+
+  account.username = checkedUsername
+  return accountsRepo.update(account)
+}

src/app/users/index.ts
@@ -6,7 +6,6 @@ export * from "./get-user"
 export * from "./login"
 export * from "./request-phone-code"
 export * from "./update-contact-alias"
-export * from "./username-available"
 
 const users = UsersRepository()
 

src/core/user-wallet.ts
@@ -102,25 +102,6 @@ export abstract class UserWallet {
   }
 
   // deprecated
-  async setUsername({ username }): Promise<true | ApplicationError> {
-    const result = await User.findOneAndUpdate(
-      { _id: this.user.id, username: null },
-      { username },
-    )
-
-    if (!result) {
-      const error = `Username is already set`
-      throw new DbError(error, {
-        forwardToClient: true,
-        logger: this.logger,
-        level: "warn",
-      })
-    }
-
-    return true
-  }
-
-  // deprecated
   async setLanguage({ language }): Promise<boolean> {
     const result = await User.findOneAndUpdate({ _id: this.user.id }, { language })
 
@@ -137,9 +118,8 @@ export abstract class UserWallet {
     return true
   }
 
-  async updateUsername({
-    username,
-  }): Promise<{ username: string | undefined; id: string }> {
+  // TODO(remove) deprecated
+  async setUsername({ username }): Promise<{ username: string | undefined; id: string }> {
     try {
       const result = await User.findOneAndUpdate(
         { _id: this.user.id, username: { $in: [null, ""] } },

src/domain/accounts/errors.ts
@@ -6,3 +6,6 @@ export class ApiKeyError extends AccountError {}
 export class ApiKeyHashError extends ApiKeyError {}
 export class InvalidApiKeyError extends ApiKeyError {}
 export class InvalidExpirationError extends ApiKeyError {}
+
+export class UsernameNotAvailableError extends AccountError {}
+export class UsernameIsImmutableError extends AccountError {}

commit 267051e712295e8cf5acaf1e0371db8efe47b18e

fix: regression from #886
username were not passed properly
aslo the mutation was not protected by the graphql shield

src/graphql/main/mutations.ts
@@ -31,9 +31,14 @@ import AccountApiKeyDisableMutation from "@graphql/root/mutation/account-api-key
 const MutationType = new GT.Object({
   name: "Mutation",
   fields: () => ({
+    // unauthed
     userRequestAuthCode: UserRequestAuthCodeMutation,
     userLogin: UserLoginMutation,
 
+    captchaCreateChallenge: CaptchaCreateChallengeMutation,
+    captchaRequestAuthCode: CaptchaRequestAuthCodeMutation,
+
+    // authed
     twoFAGenerate: TwoFAGenerateMutation,
     twoFASave: TwoFASaveMutation,
     twoFADelete: TwoFADeleteMutation,
@@ -67,9 +72,6 @@ const MutationType = new GT.Object({
     onChainAddressCurrent: OnChainAddressCurrentMutation,
     onChainPaymentSend: OnChainPaymentSendMutation,
     onChainPaymentSendAll: OnChainPaymentSendAllMutation,
-
-    captchaCreateChallenge: CaptchaCreateChallengeMutation,
-    captchaRequestAuthCode: CaptchaRequestAuthCodeMutation,
   }),
 })
 

src/graphql/main/schema.graphql
@@ -338,7 +338,7 @@ type Mutation {
   userUpdateLanguage(input: UserUpdateLanguageInput!): UserUpdateLanguagePayload!
   userUpdateUsername(input: UserUpdateUsernameInput!): UserUpdateUsernamePayload!
     @deprecated(
-      reason: "Username will be moved to @Handle in Accounts. Also SetUsername should be used instead of UpdateUpdate to reflect the idempotenty of Handles"
+      reason: "Username will be moved to @Handle in Accounts. Also SetUsername should be used instead of UpdateUsername to reflect the idempotency of Handles"
     )
 }
 

src/graphql/root/mutation/user-update-username.ts
@@ -35,6 +35,9 @@ const UserUpdateUsernameMutation = GT.Field({
 
     return {
       errors: [],
+
+      // TODO: move to accounts
+      // TODO: username and id are not populated correctly (but those properties not been used currently by a client)
       user: result,
     }
   },

src/servers/graphql-main-server.ts
@@ -28,18 +28,19 @@ export async function startApolloServerForCoreSchema() {
         accountApiKeys: isAuthenticated,
       },
       Mutation: {
-        userQuizQuestionUpdateCompleted: isAuthenticated,
-        userUpdateLanguage: isAuthenticated,
-        userContactUpdateAlias: isAuthenticated,
-
         twoFAGenerate: isAuthenticated,
         twoFASave: isAuthenticated,
         twoFADelete: isAuthenticated,
 
+        userQuizQuestionUpdateCompleted: isAuthenticated,
         deviceNotificationTokenCreate: isAuthenticated,
 
         accountApiKeyCreate: isAuthenticated,
         accountApiKeyDisable: isAuthenticated,
+
+        userUpdateUsername: isAuthenticated,
+        userUpdateLanguage: isAuthenticated,
+        userContactUpdateAlias: isAuthenticated,
 
         lnInvoiceFeeProbe: or(isAuthenticated, isApiKeyAuthenticated),
         lnNoAmountInvoiceFeeProbe: or(isAuthenticated, isApiKeyAuthenticated),

src/services/mongoose/accounts.ts
@@ -114,11 +114,12 @@ export const AccountsRepository = (): IAccountsRepository => {
     status,
     coordinates,
     title,
+    username,
   }: Account): Promise<Account | RepositoryError> => {
     try {
       const result = await User.findOneAndUpdate(
         { _id: id },
-        { level, status, coordinates, title },
+        { level, status, coordinates, title, username },
         {
           new: true,
           projection,